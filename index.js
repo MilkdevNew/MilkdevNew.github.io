ea2b64395e269001a55799be080f0152b4c2ec65
MilkdevNew <93629950+MilkdevNew@users.noreply.github.com>
2022-02-17 11:06:21 -0500

    sss
    New things

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ console.log(long);
 var msg = "Hi my name is Milk I been coding for ";
 var result = msg.concat(long, " Years");
 
-document.getElementById("AboutME").innerHTML = result;
+document.getElementsByClassName("lead my-4").innerHTML = result;
+
+function Click() {
+  window.open("https://github.com/MilkdevNew/Custom-Commands-V2");
+}
+
+function Clicke() {
+  window.open("https://github.com/MilkdevNew/Game-os");
+}
+
+function ClickAdmin() {
+  window.open("https://web.roblox.com/library/7429233006/Admin");
+}
